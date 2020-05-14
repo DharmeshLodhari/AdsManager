928dd97e011184914baec7918bf2386024c0332b
DharmeshLodhari <38317732+DharmeshLodhari@users.noreply.github.com>
2020-05-14 13:50:50 +0530

    first commit

---
+++ adsdemolibrary/src/main/java/com/example/adsdemolibrary/AdsClass.java
@@ -0,0 +1,10 @@
+package com.example.adsdemolibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class AdsClass {
+    public static void showToast(Context c){
+        Toast.makeText(c,"Hello",Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/example/demojar/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.demojar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.facebook.ads.AdSettings;
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LinearLayout adviewLayout = (LinearLayout)findViewById(R.id.banner_Main);
+        AdView adView = new AdView(this,"YOUR_PLACEMENT_ID", AdSize.BANNER_HEIGHT_50);
+        AdSettings.addTestDevice("29a2a34a-76b1-4646-a1bd-8165220fdee8");
+        AdSettings.setTestMode(true);
+        adviewLayout.addView(adView);
+        adView.loadAd();
+
+
+    }
+}
