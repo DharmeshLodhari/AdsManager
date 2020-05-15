c83fa6a48fac9a9c061577908d8c33cce4e476b3
DharmeshLodhari <38317732+DharmeshLodhari@users.noreply.github.com>
2020-05-15 11:20:34 +0530

    Code optimize added Ads Listner

--- adsdemolibrary/src/main/java/com/example/adsdemolibrary/AdsClass.java
+++ adsdemolibrary/src/main/java/com/example/adsdemolibrary/AdsClass.java
@@ -1,6 +1,7 @@
 package com.example.adsdemolibrary;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -8,6 +9,7 @@ import android.widget.Toast;
 import com.facebook.ads.AbstractAdListener;
 import com.facebook.ads.Ad;
 import com.facebook.ads.AdError;
+import com.facebook.ads.AdListener;
 import com.facebook.ads.AdSettings;
 import com.facebook.ads.AdSize;
 import com.facebook.ads.AdView;
@@ -15,12 +17,40 @@ import com.facebook.ads.InterstitialAd;
 
 public class AdsClass {
     public static String testDeviceId ="";
+   static String LOG ="Facebook_Ads";
     public static void showFBBannner(Context context, String placementID, View view,boolean testModeOn){
         AdView adView = new AdView(context,placementID, AdSize.BANNER_HEIGHT_50);
-        AdSettings.addTestDevice(testDeviceId);
+       if(!testDeviceId.isEmpty()){
+           AdSettings.addTestDevice(testDeviceId);
+       }
         AdSettings.setTestMode(testModeOn);
         ((LinearLayout)view).addView(adView);
-        adView.loadAd();
+
+        AdListener adListener = new AdListener() {
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                Log.v(LOG,"onError ");
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                Log.v(LOG,"onAdLoaded ");
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                Log.v(LOG,"onAdClicked ");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                Log.v(LOG,"onLoggingImpression ");
+            }
+        };
+        AdView.AdViewLoadConfig loadAdConfig = adView.buildLoadAdConfig()
+                .withAdListener(adListener)
+                .build();
+        adView.loadAd(loadAdConfig);
     }
     public static void showFBInterstitialAds(Context context,String placementId,boolean testModeOn){
         final InterstitialAd interstitialAd = new InterstitialAd(context,placementId);
